a770ef3a2cc49aaefd9a17097d855de5c770ae58
TkachenkoVlad <tkachenko94ua@gmail.com>
2016-04-09 10:37:04 +0300

    Created new class TkachenkoVlad

---
+++ BA.MyFirstTeamProject/Students/TkachenkoVlad.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BA.MyFirstTeamProject.Students
+{
+    class TkachenkoVlad : IStudent
+    {
+        public string Email
+        {
+            get
+            {
+                return "tkachenko94ua@gmail.com";
+            }
+        }
+
+        public string Firstname
+        {
+            get
+            {
+                return "Vlad";
+            }
+        }
+
+        public string Lastname
+        {
+            get
+            {
+                return "Tkachenko";
+            }
+        }
+    }
+}
