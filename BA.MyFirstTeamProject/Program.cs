81dcd0d1aca6671839abfee50f6d78ae1ebef48e
Dmytro <dimonser147@gmail.com>
2016-04-09 11:43:27 +0300

    Refactoring, added students amount,  remove duplicates

--- BA.MyFirstTeamProject/Program.cs
+++ BA.MyFirstTeamProject/Program.cs
@@ -8,56 +8,40 @@ using BA.MyFirstTeamProject.Utilities;
 
 namespace BA.MyFirstTeamProject
 {
-
-
-
-
-
     class Program
     {
         static void Main(string[] args)
         {
-            IStudent me = new DmytroObolonyk();
+            // TODO: 1. create students manager
+            IStudentsManager StudentsManager = new FakeStudentsManager();
+            // TODO: 2. create appropriate students collection and fill it by other students
+            IList<IStudent> students = new List<IStudent>();
             IStudent VladTk = new TkachenkoVlad();
             IStudent maxPrs = new MaksymPereshybkin();
             IStudent antonPluzhnikov = new AntonPluzhnikov();
             IStudent ivanSakharchuk = new IvanSakharchuk();
             IStudent igorAlborov = new IgorAlborovStudent();
-
-
             IStudent alkovychSerhii = new AlkovychSerhii();
-            // TODO: 1. create students manager
-            IStudentsManager StudentsManager = new FakeStudentsManager();
-
-            // TODO: 2. create appropriate students collection and fill it by other students
-            IList<IStudent> students = new List<IStudent>();
             students.Add(antonPluzhnikov);
             students.Add(ivanSakharchuk);
             students.Add(igorAlborov);
             students.Add(new YurySviridov());
-
             students.Add(new PetroReznikStudent());
             students.Add(new AndreyDzyuba());
             students.Add(VladTk);
-            students.Add(new OleksiiGorishnii());
-            
-            students.Add(new OleksiiGorishnii());
+            students.Add(new OleksiiGorishnii());            
             students.Add(new AndreiMatveev());
             students.Add(new YevhenKhatylaiev());
             students.Add(new VasiliyCherniatyn());
-            students.Add(new AndreyDzyuba());
-            students.Add(new VasiliyCherniatyn());
-            students.Add(new AndreyDzyuba());
             students.Add(new GlushenkoBorys());
-            students.Add(new OleksiiGorishnii());
-            students.Add(vadikKorzh);
+            students.Add(new VadikKorzh());
             students.Add(maxPrs);
-
             // TODO: 3. print marks of all students
             foreach (var item in students)
             {
                 Console.WriteLine(item.ToMarkString(StudentsManager.GetMark(item)));
             }
+            Console.WriteLine(students.Count);
             Console.ReadLine();
         }
     }
