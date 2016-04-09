d346fc2fbeb50690903c23e987db6997d379e56e
Сергей <respekt6666@yandex.ru>
2016-04-09 10:52:08 +0300

    Added a new class AlkovychSerhii.

---
+++ BA.MyFirstTeamProject/Students/AlkovychSerhii.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BA.MyFirstTeamProject.Students
+{
+    class AlkovychSerhii : IStudent
+    {
+        public string Email
+        {
+            get
+            {
+                return "loost1k22@gmail.com";
+            }
+        }
+
+        public string Firstname
+        {
+            get
+            {
+                return "Serhii";
+            }
+        }
+
+        public string Lastname
+        {
+            get
+            {
+                return "Alkovych";
+            }
+        }
+    }
+}
