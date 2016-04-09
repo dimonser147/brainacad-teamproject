4c4fb61361e69d10a15f7f85d2e53e099b698a32
Evgen Gladchenko <gladchenkoevgen@gmail.com>
2016-04-09 10:48:44 +0300

    Yury Sviridov add class

---
+++ BA.MyFirstTeamProject/Students/YurySviridov.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BA.MyFirstTeamProject.Students
+{
+    class YurySviridov : IStudent
+    {
+        public string Email
+        {
+            get
+            {
+                return "ferro5@inbox.ru";
+            }
+        }
+
+        public string Firstname
+        {
+            get
+            {
+                return "Yury";
+            }
+        }
+
+        public string Lastname
+        {
+            get
+            {
+                return "Sviridov";
+            }
+        }
+    }
+}
