7b8540836e42cd465fe7faffd4bba00254e3bbb9
TkachenkoVlad <tkachenko94ua@gmail.com>
2016-04-09 11:29:13 +0300

    I have no idea what's going on

--- BA.MyFirstTeamProject/Program.cs
+++ BA.MyFirstTeamProject/Program.cs
@@ -13,22 +13,15 @@ namespace BA.MyFirstTeamProject
         static void Main(string[] args)
         {
             IStudent me = new DmytroObolonyk();
-
             IStudent VladTk = new TkachenkoVlad();
-
-
             IStudent antonPluzhnikov = new AntonPluzhnikov();
             IStudent ivanSakharchuk = new IvanSakharchuk();
             IStudent igorAlborov = new IgorAlborovStudent();
-            IStudent oleksiiGorishnii = new OleksiiGorishnii();
-            IStudent andreiMatveev = new AndreiMatveev();
-
-
 
             // TODO: 1. create students manager
             IStudentsManager StudentsManager = new FakeStudentsManager();
 
-            
+
             // TODO: 2. create appropriate students collection and fill it by other students
             IList<IStudent> students = new List<IStudent>();
             students.Add(antonPluzhnikov);
@@ -36,11 +29,15 @@ namespace BA.MyFirstTeamProject
             students.Add(igorAlborov);
             students.Add(new PetroReznikStudent());
             students.Add(new AndreyDzyuba());
-            students.Add(oleksiiGorishnii);
-            students.Add(andreiMatveev);
             students.Add(VladTk);
-
+            students.Add(new OleksiiGorishnii());
+            
             // TODO: 3. print marks of all students
+            foreach (var item in students)
+            {
+                Console.WriteLine(item.ToMarkString(StudentsManager.GetMark(item)));
+            }
+            Console.ReadLine();
         }
     }
 }
