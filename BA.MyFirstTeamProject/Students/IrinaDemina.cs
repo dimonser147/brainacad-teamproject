4d25afa963e47c3c0c6e39cdc7cb7296131a3547
admin <admin@DESKTOP-QUFHSS5>
2016-07-09 10:47:21 +0300

    create class IrinaDemina

---
+++ BA.MyFirstTeamProject/Students/IrinaDemina.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BA.MyFirstTeamProject.Students
+{
+    class IrinaDemina
+    {
+        public string Email
+        {
+            get
+            {
+                return "iriinademina@gmail.com";
+            }
+        }
+
+        public string Firstname
+        {
+            get
+            {
+                return "Irina";
+            }
+        }
+
+        public string Lastname
+        {
+            get
+            {
+                return "Demina";
+            }
+        }
+    }
+}
