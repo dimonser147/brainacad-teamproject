5e7069a2fa82455627e9c10393808f0c966bd438
Dmitriy Kashkarov <dimakashkarov@gmail.com>
2016-07-09 11:14:09 +0300

    add by Dmitriy Kashkarov

--- BA.MyFirstTeamProject/Program.cs
+++ BA.MyFirstTeamProject/Program.cs
@@ -21,6 +21,7 @@ namespace BA.MyFirstTeamProject
             IStudent[] studentsCollection = new IStudent[15];
             studentsCollection[0] = new AntonBabich();
             studentsCollection[1] = new SelivanovskyIvan();
+            studentsCollection[13] = new DmytriyKashkarov();
 
             // TODO: 3. print marks of all students
 
