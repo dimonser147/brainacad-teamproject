8d1ee00fd2d4928b2258853b68bdd2dc52d5fae0
Ivan Selivanovsky <selivanovsky@mail.ua>
2016-07-09 10:20:52 +0300

    created by Selivanovsky

---
+++ BA.MyFirstTeamProject/Students/SelivanovskyIvan.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BA.MyFirstTeamProject.Students
+{
+    class SelivanovskyIvan : IStudent
+    {
+        public string Email
+        {
+            get
+            {
+                return "selivanovsky@mail.ua";
+            }
+        }
+
+        public string Firstname
+        {
+            get
+            {
+                return "Ivan";
+            }
+        }
+
+        public string Lastname
+        {
+            get
+            {
+                return "Selivanovsky";
+            }
+        }
+    }
+}
