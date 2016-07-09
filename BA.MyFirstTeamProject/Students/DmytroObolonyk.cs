3439e111f30e3661c28f8459a27a0e43d0d9dff7
BrainAcademy <C_Sharp@TRAINER_PC>
2016-07-09 10:01:32 +0300

    create Dmytro Obolonyk class

---
+++ BA.MyFirstTeamProject/Students/DmytroObolonyk.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BA.MyFirstTeamProject.Students
+{
+    public class DmytroObolonyk : IStudent
+    {
+        public string Email
+        {
+            get
+            {
+                return "dima@gmail.com";
+            }
+        }
+
+        public string Firstname
+        {
+            get
+            {
+                return "name";
+            }
+        }
+
+        public string Lastname
+        {
+            get
+            {
+                return "lastname";
+            }
+        }
+    }
+}
